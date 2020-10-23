692b693e2fcaaddf704e8a49b8dde88874568c39
spmohansp <spmohansp@gmail.com>
2020-10-23 10:21:20 +0530

    templete disign changes updated

--- model/Market.php
+++ model/Market.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Market extends Model
 {
-    //
+    public function Booking($owner_id){
+        try {
+            $Data['Shop_Details']=Owner::findorfail($owner_id);
+            return view('customer.Booking.Add_Booking',$Data);
+        } catch (\Exception $e) {
+            return back()->with('danger', 'Something went wrong!');
+        }
+    }
 }
